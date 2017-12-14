e3c93567b7284537391cce0cdcd77f2dc5b2f7d0
Alessandro Martani <alessandro.martani@gmail.com>
2017-12-14 12:42:39 +0100

    creato servizio di ricerca

--- src/main/java/io/ticketcoin/dashboard/persistence/dao/EventDAO.java
+++ src/main/java/io/ticketcoin/dashboard/persistence/dao/EventDAO.java
@@ -1,9 +1,16 @@
 package io.ticketcoin.dashboard.persistence.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 
+import io.ticketcoin.dashboard.persistence.filter.EventFilter;
 import io.ticketcoin.dashboard.persistence.model.Event;
 import io.ticketcoin.dashboard.persistence.model.User;
 import io.ticketcoin.dashboard.utils.HibernateUtils;
@@ -21,6 +28,27 @@ public class EventDAO extends GenericDAO<Event>{
 				.add(Restrictions.eq("organization", user.getOrganization())).list();
 
 	}
+
+	public List<Event> searchEvents(EventFilter filter) {
+		Criteria c = HibernateUtils.getSessionFactory().getCurrentSession()
+				.createCriteria(Event.class);
+				
+		
+		List<Criterion> x = new ArrayList<Criterion>();
+		for(String s : new StringUtils().split(filter.getGenericTxt()," "))
+		{
+		
+					x.add(Restrictions.like("name", filter.getGenericTxt(), MatchMode.ANYWHERE).ignoreCase());
+					x.add(Restrictions.like("description", filter.getGenericTxt(), MatchMode.ANYWHERE).ignoreCase());
+		}
+		
+		c.add(Restrictions.or(x.toArray(new Criterion[] {})));
+		 
+		 return c.list();
+	}
+	
+	
+	
 
 
 }
